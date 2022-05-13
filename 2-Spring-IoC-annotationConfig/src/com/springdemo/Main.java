d3349cea24f179bb28871f272ebeabcf06851e35
SelçukBaşak <selcukbasak5@gmail.com>
2022-05-13 15:55:45 +0300

    Update Main.java

--- 2-Spring-IoC-annotationConfig/src/com/springdemo/Main.java
+++ 2-Spring-IoC-annotationConfig/src/com/springdemo/Main.java
@@ -7,23 +7,22 @@ public class Main {
 	public static void main(String[] args) {
 
 		/*
-		 * ARTIK XML KALDIRDIK BU PROJEDE XML İLE İŞİMİZ BİTTİ
-		 * BU KONUDA SADECE XML İLE SCAN YAPIP ANOTATION OLANLARI BULDUK
+		 * 
 		ClassPathXmlApplicationContext context = 
 		new ClassPathXmlApplicationContext("applicationContext.xml"); 
-		*/
 		
-		//IocConfig java biz oluşturduk artık ordan calışacak xml den değil 
+		ICustomerDal customerDal = context.getBean("databaseX", ICustomerDal.class);
+		customerDal.add();
+		*/
+	
+		
+		// BU KONUDA SADECE XML İLE SCAN YAPIP ANOTATION OLANLARI BULDUK
+		// IocConfig.class biz oluşturduk artık ordan calışacak xml den değil 
 		AnnotationConfigApplicationContext context = 
 		new AnnotationConfigApplicationContext(IocConfig.class);
 		
 		
-
-	//	ICustomerDal customerDal = context.getBean("databaseX", ICustomerDal.class);
-	//	customerDal.add();
-		
 		// customer service eklendikten sonra 	
-		
 		ICustomerService customerService = context.getBean("serviceX", ICustomerService.class);
 		customerService.add();
 	}
@@ -31,10 +30,3 @@ public class Main {
 }
 
 
-
-
-/*
-IoC - Inversion of Control
-Dependency Injection 
-SOLİD yapı değiştimi eski koda dokunma
-*/
